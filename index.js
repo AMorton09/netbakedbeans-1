6b267035da3093c6a016d41d8eabe96685d9f0e7
John Pizzitola <johnpizzitola@Johns-iMac.local>
2017-04-11 19:48:12 -0500

    Got a welcome page loading

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ var express= require('express');
 
 var app = express();
 
+// keeps server info out of header
+app.disable('x-powered-by');
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'})
+;
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
 app.set('port',process.env.PORT || 3000);
+
+//do more imports here
+
+
+
+app.use(express.static(__dirname + '/public'));
+app.get('/',function(req,res){
+  res.render('home');
+});
 
 app.get('/',function(req,res){
   res.send('Express Works');
@@ -10,6 +26,6 @@ app.get('/',function(req,res){
 });
 
 app.listen(app.get('port'), function(){
-  console.log('Express started press Ctrl-C to terminate');
+  console.log("Express started on http://localhost:" + app.get('port') + ' press Ctrl-C to terminate');
 
 });
