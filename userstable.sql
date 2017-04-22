79c95fc9dfb4bcdc1c9e727be59c6d723bfdd23a
Andrew Morton <morton09@me.com>
2017-04-21 23:20:37 -0500

    added sql table script

---
+++ userstable.sql
@@ -0,0 +1,15 @@
+USE sakila;
+
+CREATE TABLE users (
+ customer_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+ fname varchar(50),
+ lname varchar(50),
+ addr varchar(50),
+ email varchar(50),
+ password varchar(50),
+ ccnum varchar(50),
+ expdate varchar(50),
+ ccv varchar(50),
+ admin int,
+ PRIMARY KEY  (customer_id)
+);
