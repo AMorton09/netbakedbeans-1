6696d1d7d6b22f98512c358d9c54265d5143168f
John Pizzitola <jvpizzitola@gmail.com>
2017-04-25 21:32:32 -0500

    index

--- index.js
+++ index.js
@@ -27,155 +27,16 @@ function getModel() {
   return require(`./model-cloudsql`);
 }
 
+
+
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res) {
-  res.redirect("login");
-});
 
-app.post("/removefromcart", (req, res, next) => {
-      var cartID = req.body;
-      console.log(cartID);
-      getModel().removeFromCart(cartID,(error, savedData) =>{
+//_______________________
+//|                     |
+//|      APP.POST       |
+//|_____________________|
 
-  });
-      console.log("i ran here");
-      res.redirect(`cart`);
-});
-
-app.post("/removemovie", (req, res, next) => {
-      var film_id = req.body;
-      console.log(film_id);
-      getModel().deleteMovie(film_id,(error, results) =>{
-        console.log(results);
-  });
-      console.log("i ran here");
-      res.redirect(`admin-movies`);
-});
-
-app.post("/removeuser", (req, res, next) => {
-      var customerID = req.body;
-      console.log(customerID);
-      getModel().deleteUser(customerID,(error, results) =>{
-        console.log(results);
-  });
-      console.log("i ran here");
-
-      res.redirect(`allusers`);
-});
-
-app.post("/search", (req, res, next) => {
-      var searchTerm = req.body;
-
-      console.log(searchTerm);
-      getModel().search(searchTerm, (err, results) => {
-    if (err) {
-      console.log(err);
-      next(err);
-      return;
-    }
-
-    console.log(results);
-     res.render("movies", {movies: results});
-
-  });
-
-});
-
-
-
-
-app.get("/addmovie", function(req, res) {
-  res.render("addmovie");
-});
-
-
-app.get("/customer", function(req, res) {
-  res.render("customer");
-});
-
-app.get("/checkout", function(req, res) {
-  res.render("checkout");
-});
-
-app.get("/login", function(req, res) {
-
-  res.render("login");
-});
-
-
-
-
-
-app.post("/loginAuth", (req, res, next) => {
- var loginFormData = req.body;
-
-  getModel().loginAuth(loginFormData, (error, results) => {
-
-      if(error){
-      console.log("search DATA:====");
-      console.log(error);
-      res.render(`loginUnsuccessful`);
-
-  }
-  else{
-
-
-
-    res.cookie("userinfo",results, { expire: new Date() + 9999 })
-
-    if (results.admin == 0){
-      console.log("i ran so far away");
-    res.render('customer');
-  }
-  else if (results.admin == 1){
-    res.render('admin');
-  }
-  }
-  });
-});
-
-app.get("/addemployee", function(req, res) {
-  res.render("addemployee");
-});
-
-app.get("/welcome", function(req, res) {
-  res.render("customer");
-});
-
-app.get("/register", function(req, res) {
-  res.render("register");
-});
-
-app.use(function(req, res, next) {
-  console.log("Looking for URL : " + req.url);
-  next();
-});
-
-
-app.use(function(err, req, res, next) {
-  console.log("Error : " + err.message);
-  next();
-});
-
-app.get("/about", function(req, res) {
-  // Point at the about.handlebars view
-  // Allow for the test specified in tests-about.js
-  res.render("about");
-});
-app.get("/logout", function(req, res) {
-  res.render("login");
-});
-
-app.get("/contact", function(req, res) {
-  // CSRF tokens are generated in cookie and form data and
-  // then they are verified when the user posts
-  res.render("contact", { csrf: "CSRF token here" });
-});
-
-app.get("/thankyou", function(req, res) {
-  res.render("thankyou");
-});
 
 app.post("/process", function(req, res) {
   console.log("Form : " + req.query.form);
@@ -185,13 +46,7 @@ app.post("/process", function(req, res) {
   res.redirect(303, "/thankyou");
 });
 
-app.get("/file-upload", function(req, res) {
-  var now = new Date();
-  res.render("file-upload", {
-    year: now.getFullYear(),
-    month: now.getMonth()
-  });
-});
+
 
 app.post("/register", (req, res, next) => {
   var registerFormData = req.body;
@@ -253,6 +108,177 @@ app.post("/addmovie", (req, res, next) => {
   });
   res.redirect(`login`);
 });
+
+
+
+
+app.post("/file-upload/:year/:month", function(req, res) {
+  // Parse a file that was uploaded
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, file) {
+    if (err) return res.redirect(303, "/error");
+    console.log("Received File");
+
+    // Output file information
+    console.log(file);
+    res.redirect(303, "/thankyou");
+  });
+});
+
+app.post("/removefromcart", (req, res, next) => {
+      var cartID = req.body;
+      console.log(cartID);
+      getModel().removeFromCart(cartID,(error, savedData) =>{
+
+  });
+      console.log("i ran here");
+      res.redirect(`cart`);
+});
+
+app.post("/removemovie", (req, res, next) => {
+      var film_id = req.body;
+      console.log(film_id);
+      getModel().deleteMovie(film_id,(error, results) =>{
+        console.log(results);
+  });
+      console.log("i ran here");
+      res.redirect(`admin-movies`);
+});
+
+app.post("/removeuser", (req, res, next) => {
+      var customerID = req.body;
+      console.log(customerID);
+      getModel().deleteUser(customerID,(error, results) =>{
+        console.log(results);
+  });
+      console.log("i ran here");
+
+      res.redirect(`allusers`);
+});
+
+
+app.post("/search", (req, res, next) => {
+      var searchTerm = req.body;
+
+      console.log(searchTerm);
+      getModel().search(searchTerm, (err, results) => {
+    if (err) {
+      console.log(err);
+      next(err);
+      return;
+    }
+
+    console.log(results);
+     res.render("movies", {movies: results});
+
+  });
+
+});
+app.post("/loginAuth", (req, res, next) => {
+ var loginFormData = req.body;
+
+  getModel().loginAuth(loginFormData, (error, results) => {
+
+      if(error){
+      console.log("search DATA:====");
+      console.log(error);
+      res.render(`loginUnsuccessful`);
+
+  }
+  else{
+
+
+
+    res.cookie("userinfo",results, { expire: new Date() + 9999 })
+
+    if (results.admin == 0){
+      console.log("i ran so far away");
+    res.render('customer');
+  }
+  else if (results.admin == 1){
+    res.render('admin');
+  }
+  }
+  });
+});
+
+
+
+//_______________________
+//|                     |
+//|      APP.GET        |
+//|_____________________|
+
+
+app.get("/", function(req, res) {
+  res.redirect("login");
+});
+
+app.get("/addmovie", function(req, res) {
+  res.render("addmovie");
+});
+
+app.get("/admin-editmovies", function(req, res) {
+  res.render("admin-editmovies");
+});
+
+app.get("/admin-editusers", function(req, res) {
+  res.render("admin-editusers");
+});
+
+app.get("/admin-home", function(req, res) {
+  res.render("admin-home");
+});
+
+app.get("/admin-reports", function(req, res) {
+  res.render("admin-reports");
+});
+
+
+app.get("/customer", function(req, res) {
+  res.render("customer");
+});
+
+app.get("/checkout", function(req, res) {
+  res.render("checkout");
+});
+
+app.get("/login", function(req, res) {
+
+  res.render("login");
+});
+
+app.get("/addemployee", function(req, res) {
+  res.render("addemployee");
+});
+
+app.get("/welcome", function(req, res) {
+  res.render("customer");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.get("/about", function(req, res) {
+  // Point at the about.handlebars view
+  // Allow for the test specified in tests-about.js
+  res.render("about");
+});
+app.get("/logout", function(req, res) {
+  res.render("login");
+});
+
+app.get("/contact", function(req, res) {
+  // CSRF tokens are generated in cookie and form data and
+  // then they are verified when the user posts
+  res.render("contact", { csrf: "CSRF token here" });
+});
+
+app.get("/thankyou", function(req, res) {
+  res.render("thankyou");
+});
+
 
 
 app.get('/allusers', (req, res, next) => {
@@ -321,20 +347,6 @@ app.get('/cart', (req, res, next) => {
   });
 });
 
-app.post("/file-upload/:year/:month", function(req, res) {
-  // Parse a file that was uploaded
-  var form = new formidable.IncomingForm();
-  form.parse(req, function(err, fields, file) {
-    if (err) return res.redirect(303, "/error");
-    console.log("Received File");
-
-    // Output file information
-    console.log(file);
-    res.redirect(303, "/thankyou");
-  });
-});
-
-// Demonstrate how to set a cookie
 app.get("/nothing", function(req, res) {
   // Set the key and value as well as expiration
   res
@@ -353,6 +365,104 @@ app.get("/deletecookie", function(req, res) {
   res.clearCookie("username");
   res.send("username Cookie Deleted");
 });
+
+app.get("/file-upload", function(req, res) {
+  var now = new Date();
+  res.render("file-upload", {
+    year: now.getFullYear(),
+    month: now.getMonth()
+  });
+});
+app.get("/viewcount", function(req, res, next) {
+  res.send(
+    "You viewed this page " + req.session.views["/viewcount"] + " times "
+  );
+});
+
+// Reading and writing to the file system
+// Import the File System module : npm install --save fs
+var fs = require("fs");
+
+app.get("/readfile", function(req, res, next) {
+  // Read the file provided and either return the contents
+  // in data or an err
+  fs.readFile("./public/randomfile.txt", function(err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    res.send("The File : " + data.toString());
+  });
+});
+app.get("/writefile", function(req, res, next) {
+  // If the file doesn't exist it is created and then you add
+  // the text provided in the 2nd parameter
+  fs.writeFile("./public/randomfile2.txt", "More random text", function(err) {
+    if (err) {
+      return console.error(err);
+    }
+  });
+
+  // Read the file like before
+  fs.readFile("./public/randomfile2.txt", function(err, data) {
+    if (err) {
+      return console.error(err);
+    }
+
+    res.send("The File : " + data.toString());
+  });
+});
+
+//_______________________
+//|                     |
+//|      APP.USE        |
+//|_____________________|
+
+// Defines a custom 404 Page and we use app.use because
+// the request didn't match a route (Must follow the routes)
+app.use(function(req, res) {
+  // Define the content type
+  res.type("text/html");
+
+  // The default status is 200
+  res.status(404);
+
+  // Point at the 404.handlebars view
+  res.render("404");
+});
+
+// Custom 500 Page
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+
+  // Point at the 500.handlebars view
+  res.render("500");
+});
+
+app.use((err, req, res, next) => {
+  // Format error and forward to generic error handler for logging and
+  // responding to the request
+  err.response = err.message;
+  console.log(err.message);
+  next(err);
+});
+
+app.use(function(req, res, next) {
+  console.log("Looking for URL : " + req.url);
+  next();
+});
+
+
+app.use(function(err, req, res, next) {
+  console.log("Error : " + err.message);
+  next();
+});
+
+
+
+
+// Demonstrate how to set a cookie
+
 
 // Storing session information can be done in a few ways.
 // For development we can work with a memory store
@@ -399,73 +509,10 @@ app.use(function(req, res, next) {
 
 // When this page is accessed get the correct value from
 // the views array
-app.get("/viewcount", function(req, res, next) {
-  res.send(
-    "You viewed this page " + req.session.views["/viewcount"] + " times "
-  );
-});
 
-// Reading and writing to the file system
-// Import the File System module : npm install --save fs
-var fs = require("fs");
 
-app.get("/readfile", function(req, res, next) {
-  // Read the file provided and either return the contents
-  // in data or an err
-  fs.readFile("./public/randomfile.txt", function(err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    res.send("The File : " + data.toString());
-  });
-});
-app.get("/writefile", function(req, res, next) {
-  // If the file doesn't exist it is created and then you add
-  // the text provided in the 2nd parameter
-  fs.writeFile("./public/randomfile2.txt", "More random text", function(err) {
-    if (err) {
-      return console.error(err);
-    }
-  });
 
-  // Read the file like before
-  fs.readFile("./public/randomfile2.txt", function(err, data) {
-    if (err) {
-      return console.error(err);
-    }
 
-    res.send("The File : " + data.toString());
-  });
-});
-// Defines a custom 404 Page and we use app.use because
-// the request didn't match a route (Must follow the routes)
-app.use(function(req, res) {
-  // Define the content type
-  res.type("text/html");
-
-  // The default status is 200
-  res.status(404);
-
-  // Point at the 404.handlebars view
-  res.render("404");
-});
-
-// Custom 500 Page
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500);
-
-  // Point at the 500.handlebars view
-  res.render("500");
-});
-
-app.use((err, req, res, next) => {
-  // Format error and forward to generic error handler for logging and
-  // responding to the request
-  err.response = err.message;
-  console.log(err.message);
-  next(err);
-});
 
 app.listen(app.get("port"), function() {
   console.log(
